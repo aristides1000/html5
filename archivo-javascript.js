85df936af69998aff4280fce0a86b44ac32847c8
aristides1000 <aristides1000@gmail.com>
2018-11-21 15:10:53 -0400

    Curso HTML 5. Eventos en HTML5 con JavaScript I. Vídeo 15

--- archivo-javascript.js
+++ archivo-javascript.js
@@ -15,12 +15,47 @@ function ejecuta(){/*esto es para decirle al docuento javascript que reconozca l
 	/*var z=document.getElementsByClassName("importante")[i].onclick=saludo;*/
 	/*si queremos que agarre varios elementos con la clase específica debemos hacer un ciclo for de la siguiente manera*/
 	/*de igual forma si agarramos y eliminamos el var z agarra de todas formas*/
-	
+	/*
 	for(var i=0;i<3;i++){
 		var z=document.getElementsByClassName("importante")[i].onclick=saludo;
 	}
-	
+	*/
 	/*si no sabemos la cantidad de elementos, no hay de qué preocuparnos, ya que lo explicarán más adelante el curso*/
+
+	/*querySelector() retorna solo el primer elemento, en cambio querySelectorAll() retorna todos los elementos*/
+	/*
+	document.querySelector(".importante").onclick=saludo;
+	*/
+	/*si queremos apuntar al último elemento de la etiqueta <p> lo que debemos hacer es lo siguiente*/
+	/*document.querySelector("#principal p:last-child").onclick=saludo;*//*recordar que querySelector() sólo devuelve un elemento*/
+
+	/*si queremos devolver todos las <p> debemos hacer lo siguiente*/
+
+	/*var elementos=document.querySelectorAll("#principal p")[0].onclick=saludo;*//*hay que poner entre corchetes [] la posición del array que queremos devolver*/
+
+	/*si queremos apuntar a todos debemos hacer lo siguiente*/
+	/*
+	for(var i=0;i<4;i++){
+		var elementos=document.querySelectorAll("#principal p");
+
+		elementos[i].onclick=saludo;
+	}
+	*/
+	/*Ahora bien, lo anterior es lo mismo que esto*/
+	/*
+	for(var i=0;i<4;i++){
+		document.querySelectorAll("#principal p")[i].onclick=saludo;
+	}
+	*/
+
+	/*Ahora bien, sino sabemos la cantidad de elementos que contenga la etiqueta debemos hacer lo siguiente si no sabemos el tamaño del arreglo*/
+
+	var elementos=document.querySelectorAll("#principal p, span");/*con esto lo que le queremos decir es que va a tomar todos los elementos p que se encuentren en la clase principal más todos los elementos que se encuentren entre las etiquetas span*/
+
+	for(var i=0;i<elementos.length;i++){/*con esta instrucción ;i<elementos.length; no es necesario saber el tamaño del arreglo, sino que más bien el código lo averigua, y tenemos que hacer estos cambios en las lineas de código para que tengamos inicializada la variable*/
+
+		elementos[i].onclick=saludo;
+	}	
 }
 
 function saludo(){
