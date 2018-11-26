5a3ab3c1f7090f2663ae2e26efd95637db85106e
aristides1000 <aristides1000@gmail.com>
2018-11-26 14:45:39 -0400

    Curso HTML 5. Inserción audio. Vídeo 20

--- video/funcionalidad_barra.js
+++ video/funcionalidad_barra.js
@@ -1,4 +1,6 @@
-var mivideo, reproducir, barra, progreso;
+var mivideo, reproducir, barra, progreso, maximo;/*declaración de variables*/
+
+maximo=600;
 
 function comenzar(){
 	mivideo=document.getElementById("mivideo");
@@ -8,7 +10,7 @@ function comenzar(){
 
 	reproducir.addEventListener("click",clicando,false);
 
-	progreso.addEventListener("click",adelantando,false);
+	barra.addEventListener("click",adelantando,false);
 }
 
 function clicando(){
@@ -21,7 +23,36 @@ function clicando(){
 	else{
 		mivideo.play();
 		reproducir.innerHTML="Pause";/*la propiedad innerHTML sirve para cambiar el texto del botón, y para ello debemos hacer uso de los Id*/
+
+		bucle=setInterval(estado,30);/*esta función "setInterval()" posee 2 parámetros, 1ero va la función de estado y el 2do parámetro es la frecuencia con la que queremos llamar a esa función, la unidad de medida de esta frecuencia es en milisegundos*//*1000milisegundos es igual a 1 segundo*/
 	}
+}
+
+function estado(){
+
+	if (mivideo.ended==false){
+
+		var total=parseInt(mivideo.currentTime*maximo/mivideo.duration);
+
+		progreso.style.width=total+"px";/*el "+" es un concatenador*/
+
+	}else{
+
+		reproducir.innerHTML="Play";/*Esto es para que al finalizar el video cambie el texto del botón*/
+		
+	}
+}
+
+function adelantando(posicion){
+
+	var ratonX=posicion.pageX-barra.offsetLeft;
+
+	var nuevoTiempo=ratonX*mivideo.duration/maximo;
+
+	mivideo.currentTime=nuevoTiempo;
+
+	progreso.style.width=ratonX+"px";
+
 }
 
 window.addEventListener("load",comenzar,false);
